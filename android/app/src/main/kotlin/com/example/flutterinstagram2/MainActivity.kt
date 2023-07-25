b7ea968f949a80f5dfa2dd2624cd61f5065d26bb
mc <dyd97wns@naver.com>
2023-07-25 22:14:53 +0900

    renewall instagram

---
+++ android/app/src/main/kotlin/com/example/flutterinstagram2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterinstagram2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import './style.dart' as style;
+
+void main() {
+  runApp(
+    MaterialApp(
+        theme: style.theme,
+        home: MyApp()
+    )
+  );
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Instagram'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add_box_outlined),
+            onPressed: (){},
+            iconSize: 30,
+          )
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
